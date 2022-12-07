035ca65b98dc3fb0384f82c08420ed3eaee7698f
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-12-07 16:43:12 +0100

    hop to fix GH problem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-const axios = require('axios');
+
 const form = document.querySelector('#search-form');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
